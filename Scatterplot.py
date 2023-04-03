1bfddaabcfba31d7c1810f53bc747dbfe19ea22d
Derry <derryxu@berkeley.edu>
2023-04-03 13:37:30 -0700

    new files for player ages

--- Scatterplot.py
+++ Scatterplot.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 app = Dash(__name__)
 
-atl = pd.read_csv("team-data/Atlanta Dream(1).csv")
+# --------------- TABLE FORMATTING --------------- #
+
+atl = pd.read_csv("team-data/Atlanta Dream (1).csv")
 chi = pd.read_csv("team-data/Chicago Sky.csv")
 con = pd.read_csv("team-data/Connecticut Sun.csv")
 dal = pd.read_csv("team-data/Dallas Wings (1).csv")
@@ -12,10 +14,18 @@ lav = pd.read_csv("team-data/Las Vegas Aces (2).csv")
 la = pd.read_csv("team-data/Los Angeles Sparks.csv")
 min = pd.read_csv("team-data/Minnesota Lynx (1).csv")
 ny = pd.read_csv("team-data/New York Liberty (2).csv")
-pho = pd.read_csv("team-data/Phoneix Mercury.csv")
+pho = pd.read_csv("team-data/Phoenix Mercury.csv")
 sea = pd.read_csv("team-data/Seattle Storm.csv")
 was = pd.read_csv("team-data/Washington Mystics.csv")
 
+ages_pg1 = pd.read_html("player-data/WNBA Stats _ Players Bios1.html")
+ages_pg2 = pd.read_html("player-data/WNBA Stats _ Players Bios2.html")
+ages_pg3 = pd.read_html("player-data/WNBA Stats _ Players Bios3.html")
+ages_pg4 = pd.read_html("player-data/WNBA Stats _ Players Bios4.html")
+
+ages = pd.concat([ages_pg1[0], ages_pg2[0], ages_pg3[0], ages_pg4[0]]).reset_index()
+
+# ----------------- DASHBOARD LAYOUT ----------------- #
 
 app.layout = html.Div([
     html.Div(children='Hello World')
