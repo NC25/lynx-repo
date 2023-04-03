24ceed7225dd6a738cca4b261a2d582c299b8842
Derry <derryxu@berkeley.edu>
2023-04-02 20:23:31 -0700

    ??

--- scatterplot/main.py
+++ scatterplot/main.py
@@ -3,18 +3,18 @@ import pandas as pd
 
 app = Dash(__name__)
 
-atl = pd.read_csv("Atlanta Dream(1).csv")
-chi = pd.read_csv("Chicago Sky.csv")
-con = pd.read_csv("Connecticut Sun.csv")
-dal = pd.read_csv("Dallas Wings (1).csv")
-ind = pd.read_csv("Indiana Fever.csv")
-lav = pd.read_csv("Las Vegas Aces (2).csv")
-la = pd.read_csv("Los Angeles Sparks.csv")
-min = pd.read_csv("Minnesota Lynx (1).csv")
-ny = pd.read_csv("New York Liberty (2).csv")
-pho = pd.read_csv("Phoneix Mercury.csv")
-sea = pd.read_csv("Seattle Storm.csv")
-was = pd.read_csv("Washington Mystics.csv")
+atl = pd.read_csv("team-data/Atlanta Dream(1).csv")
+chi = pd.read_csv("team-data/Chicago Sky.csv")
+con = pd.read_csv("team-data/Connecticut Sun.csv")
+dal = pd.read_csv("team-data/Dallas Wings (1).csv")
+ind = pd.read_csv("team-data/Indiana Fever.csv")
+lav = pd.read_csv("team-data/Las Vegas Aces (2).csv")
+la = pd.read_csv("team-data/Los Angeles Sparks.csv")
+min = pd.read_csv("team-data/Minnesota Lynx (1).csv")
+ny = pd.read_csv("team-data/New York Liberty (2).csv")
+pho = pd.read_csv("team-data/Phoneix Mercury.csv")
+sea = pd.read_csv("team-data/Seattle Storm.csv")
+was = pd.read_csv("team-data/Washington Mystics.csv")
 
 
 app.layout = html.Div([
